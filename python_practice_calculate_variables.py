ca4a61f057ef6208b609ec1abe056ab18f151b79
John S. Rajeski, Ed.D <48130054+rajeski@users.noreply.github.com>
2020-08-27 21:29:20 +0900

    Variables
    Simple, Python variable calculation regarding savings.

---
+++ python_practice_calculate_variables.py
@@ -0,0 +1,16 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 03:13:28) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+# Create a variable savings
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.10
+
+# Calculate result
+years = 7
+result = (savings * growth_multiplier ** years)
+
+# Print out result
+print(result)
